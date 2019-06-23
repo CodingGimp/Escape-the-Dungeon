9d7b7ac659be64869afecd72800d63ebd2ff3287
Mario Jamid <coding.gimp@gmail.com>
2019-06-22 17:43:58 -0700

    Movement

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
@@ -7,29 +8,55 @@ CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)
 ]
 
-def get_locations():
-    monster = None
-    door = None
-    player = None
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    return monster, door, player
+def get_locations():
+    return random.sample(CELLS, 3)
 
 def move_player(player, move):
-    return player
+    x, y = player
+    if move == 'LEFT':
+        x -= 1
+    if move == 'RIGHT':
+        x += 1
+    if move == 'UP':
+        y -= 1
+    if move == 'DOWN':
+        y += 1
+
+    return x, y
 
 def get_moves(player):
     moves = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+    x, y = player
+    if x == 0:
+        moves.remove('LEFT')
+    if x == 4:
+        moves.remove('RIGHT')
+    if y == 0:
+        moves.remove('UP')
+    if y == 4:
+        moves.remove('DOWN')
 
-
+    return moves
+    
+monster, door, player = get_locations() 
 
 while True:
+    valid_moves = get_moves(player)
+    clear_screen()
     print('Welcome to the Dungeon!')
-    print('You\'re currently in room {}.')
-    print('You can move {}.')
+    print('You\'re currently in room {}.'.format(player))
+    print('You can move {}.'.format(', '.join(valid_moves)))
     print('Enter QUIT to quit.')
 
     move = input('> ').upper()
 
     if move == 'QUIT':
         break 
-    
+    if move in valid_moves:
+        player = move_player(player, move)
+    else:
+        print('\n** Invalid move. There is a wall there!\n **')
+        continue
